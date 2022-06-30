9f9bf4351306666f602a9f4fc9eba8ae1abee1c8
hasankarim18 <hallysust@gmail.com>
2022-06-30 16:47:28 +0600

    Changing layout cart title

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -9,7 +9,7 @@ const HeaderCartButton = (props) => {
             <span className={classes.icon} >
                 <CartIcon />
             </span>
-            <span> Your Cart </span>
+            <span>Visit Your Cart </span>
             <span className={classes.badge} >3</span>
         </button>
     )
